bb51ecc71f883dfc02084d5499aa5a271a6f7d5d
Santiago Baron Zuleta <santiagobazul@hotmail.com>
2024-08-26 06:45:21 -0500

    [FC] First Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
 import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+	],
+	theme: {
+		extend: {},
+		screens: {
+			sm: '400px',
+			md: '650px',
+			lg: '1000px',
+			xl: '1400px',
+		},
+		container: {
+			center: true,
+		},
+	},
+	darkMode: "class",
+	plugins: [nextui()]
 };
 export default config;
